9f32464a8684d1585a3a7fa92ffb8d52218d7687
sunteck <sildtfine@163.com>
2019-12-20 16:28:55 +0800

    upload about parts

--- product_sunteck/models/product_sparts.py
+++ product_sunteck/models/product_sparts.py
@@ -3,8 +3,9 @@ from odoo import api, fields, models
 class Sproduct(models.Model):
     _inherit = 'product.template'
     #is_available = fields.Boolean('Is Available?')
-    sale_ok=fields.Boolean('Can be sold,why?', defualt=False)
-    serialNo = fields.Char("Sn",required=True)
+    sale_ok=fields.Boolean("Can be sold?",defualt=False)
+    purchase_ok=fields.Boolean("Can be Purchased?",default=False)
+    serialNo = fields.Char("Sn",required=True,index=True)
     #publisher_id = fields.Many2one(index=True)
         
 
