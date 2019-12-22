ec7d73d1151684a6959a5e633d3858a330407d3e
sildtfine <sildtfine@outlook.com>
2019-12-22 14:40:11 +0800

    home upload 1222

--- product_sunteck/models/product_sparts.py
+++ product_sunteck/models/product_sparts.py
@@ -4,8 +4,21 @@ class Sproduct(models.Model):
     _inherit = 'product.template'
     #is_available = fields.Boolean('Is Available?')
     sale_ok=fields.Boolean("Can be sold?",defualt=False)
-    purchase_ok=fields.Boolean("Can be Purchased?",default=False)
+    #purchase_ok=fields.Boolean("Can be Purchased?",default=False)
     serialNo = fields.Char("Sn",required=True,index=True)
-    #publisher_id = fields.Many2one(index=True)
-        
+    partstype=fields.Selection([('elec','Electrical'),('stru','Struct'),('other','Others')])
 
+    @api.multi
+    def _check_sn(self):
+    	self.ensure_one()
+    	snlen=self.serialNo.len()
+    	return snlen==10
+
+    @api.multi
+    def button_check_sn(self):
+    	for part in self:
+    		if not part.serialNo:
+                raise Warning('Please provide an SN for %s' % part.name)
+            if part.serialNo and not book._check_sn():
+                raise Warning('%s is an invalid SN' % part.sn)
+            return True
