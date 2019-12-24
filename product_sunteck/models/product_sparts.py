0c0addef0b79987e5a91ebc481c0da72d2ab0532
sunteck <sildtfine@163.com>
2019-12-24 12:52:59 +0800

    marktwint xpath 1224

--- product_sunteck/models/product_sparts.py
+++ product_sunteck/models/product_sparts.py
@@ -3,7 +3,7 @@ from odoo import api, fields, models
 class Sproduct(models.Model):
     _inherit = 'product.template'
     #is_available = fields.Boolean('Is Available?')
-    sale_ok=fields.Boolean("Can be sold?",defualt=False)
+    #sale_ok=fields.Boolean("Can be sold?",defualt=False)
     #purchase_ok=fields.Boolean("Can be Purchased?",default=False)
     serialNo = fields.Char("Sn",required=True,index=True)
     partstype=fields.Selection([('elec','Electrical'),('stru','Struct'),('other','Others')])
